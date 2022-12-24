a1764db49a69ada5482bb6aa54432447d48a15e5
Gustavo <gustavooliveirarei35@gmail.com>
2022-12-24 20:18:06 -0300

    add django-mptt

--- basket/basket.py
+++ basket/basket.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from store.models import Product
 
 
-class Basket():
+class Basket:
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get(settings.BASKET_SESSION_ID)
@@ -20,40 +20,40 @@ class Basket():
         product_id = str(product.id)
 
         if product_id in self.basket:
-            self.basket[product_id]['qty'] = qty
+            self.basket[product_id]["qty"] = qty
         else:
-            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+            self.basket[product_id] = {"price": str(product.discount_price), "qty": qty}
 
         self.save()
 
     def __iter__(self):
         product_ids = self.basket.keys()
-        products = Product.products.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids)
         basket = self.basket.copy()
 
         for product in products:
-            basket[str(product.id)]['product'] = product
+            basket[str(product.id)]["product"] = product
 
         for item in basket.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['qty']
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
             yield item
 
     def __len__(self):
-        return sum(item['qty'] for item in self.basket.values())
+        return sum(item["qty"] for item in self.basket.values())
 
     def update(self, product, qty):
         product_id = str(product)
         if product_id in self.basket:
-            self.basket[product_id]['qty'] = qty
+            self.basket[product_id]["qty"] = qty
         self.save()
 
     def get_subtotal_price(self):
-        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+        return sum(Decimal(item["price"]) * item["qty"] for item in self.basket.values())
 
     def get_total_price(self):
 
-        subtotal = sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+        subtotal = sum(Decimal(item["price"]) * item["qty"] for item in self.basket.values())
 
         if subtotal == 0:
             shipping = Decimal(0.00)
--- book/views.py
+++ book/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from store.models import *
 
+
 def book_list(request):
     book = Product.objects.all()
-    livro = Product.products.all()
+    livro = Product.objects.all()
     return render(request, "store/book.html", {"book": book, "livro": livro})
